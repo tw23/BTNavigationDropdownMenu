e3dc2b491e5e40b9628893c80fdb3a7f1c93b046
Tim Wong <twong2323@gmail.com>
2017-09-13 12:29:47 -0700

    feat(carthage): added carthage ability

---
+++ BTNavigationDropdownMenu.framework/Headers/BTNavigationDropdownMenu.h
@@ -0,0 +1,19 @@
+//
+//  BTNavigationDropdownMenu.h
+//  BTNavigationDropdownMenu
+//
+//  Created by Tim Wong on 9/13/17.
+//  Copyright © 2017 tw23. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BTNavigationDropdownMenu.
+FOUNDATION_EXPORT double BTNavigationDropdownMenuVersionNumber;
+
+//! Project version string for BTNavigationDropdownMenu.
+FOUNDATION_EXPORT const unsigned char BTNavigationDropdownMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTNavigationDropdownMenu/PublicHeader.h>
+
+
--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import BTNavigationDropdownMenu
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var selectedCellLabel: UILabel!
